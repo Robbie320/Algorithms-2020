2afd7a5786b4e29e80a8ba094a257e3a54ee5f0f
Robbie Perrone <49956805+Robbie320@users.noreply.github.com>
2020-09-28 23:03:11 -0400

    Create QuickSort.java

---
+++ Assignment 2/QuickSort.java
@@ -0,0 +1,20 @@
+public class QuickSort {
+	public int quickSort(String[] array) {
+		int comparisons = 0;
+		int n = array.length;
+		
+		for(int i = 0; i < 0; i++) {
+		
+		}
+		
+		System.out.println("Quick Sort:");
+		//print out array to check
+		for(int x = 0; x < n; x++) {
+			System.out.println(array[x]);
+		}
+		System.out.println();
+		//number of comparisons done during sort
+		System.out.println("Comparisons:");
+		return comparisons;
+	}//end quickSort
+}//end class QuickSort
