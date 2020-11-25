192f11b63618b69c02c9b75b84958a807c542ebc
Robbie Perrone <49956805+Robbie320@users.noreply.github.com>
2020-11-24 20:27:18 -0500

    Create Edge.java

---
+++ Assignment 5/Edge.java
@@ -0,0 +1,15 @@
+public class Edge {
+	Node destination;
+	int weight;
+	
+	public Edge(Node destination, int weight) {
+		this.destination = destination;
+		this.weight = weight;
+	}
+	public Node getDestination() {
+		return destination;
+	}
+	public int getWeight() {
+		return weight;
+	}
+}
