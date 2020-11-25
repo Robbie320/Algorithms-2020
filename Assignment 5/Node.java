1f242985d0cbffeef473ffbd8bb60278c864180c
Robbie Perrone <49956805+Robbie320@users.noreply.github.com>
2020-11-24 20:27:56 -0500

    Create Node.java

---
+++ Assignment 5/Node.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Node {
+    //For Linked List
+    public Node head;
+    public Node next;
+    public Node prev;
+    //public String item;
+    
+    //For Graph Linked Objects
+    public int id;
+    //public boolean hasBeenProcessed;
+    //public boolean markedForProcessing;
+    public ArrayList<Node> neighbors = new ArrayList<>();
+    
+    //For Single Source Shortest Path (SSSP)
+    public ArrayList<Node> vertices = new ArrayList<>();
+    public ArrayList<Object> directedEdges = new ArrayList<>();
+    int distance;
+    Node prevVertex;
+    
+    //Knapsack - Spice
+    public String color;
+    public int total_price;
+    public int quantity;
+    public int unit_price;
+}//end class Node
