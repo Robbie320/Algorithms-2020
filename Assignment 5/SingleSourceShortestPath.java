13288df87fc5fa1c95ca72e32679591ab50a808f
Robbie Perrone <49956805+Robbie320@users.noreply.github.com>
2020-11-25 18:17:11 -0500

    Update SingleSourceShortestPath.java

--- Assignment 5/SingleSourceShortestPath.java
+++ Assignment 5/SingleSourceShortestPath.java
@@ -16,9 +16,9 @@ public class SingleSourceShortestPath {
 			Node vertex = graphLinkedList.search(i);
 			vertex.distance = Integer.MAX_VALUE;
 			vertex.prevVertex = null;
-		}
+		}//end for i
 		sourceVertex.distance = 0;
-	}
+	}//end initSingleSource
 	public void bellmanFord(LinkedList graphLinkedList, Node sourceVertex, int vertexCount, int edgeCount) {
 		/*Bellman-Ford(G,w,s)
 		* 	Init-Single-Source(G,s)
@@ -30,28 +30,33 @@ public class SingleSourceShortestPath {
 		* 			return FALSE
 		* 	return TRUE */
 		this.initSingleSource(graphLinkedList, sourceVertex, vertexCount);
-		for(int i = sourceVertex.id; i < (vertexCount-1); i++) {
-			Node source = graphLinkedList.search(i);
-			for(int j = 0; j < edgeCount; j++) {
-				this.relax(source, source.neighbors);
-			}
-		}
-		for(int j = 0; j < edgeCount; j++) {
-			/*if() {
-				return false;
-			}*/
-		}
-	}
-	public void relax(Node source, Node destination) {
+		
+		Node source = null;
+		boolean relax;
+		for(int i = 1 /*sourceVertex.id*/; i < (vertexCount-1); i++) {
+			for(int t = 1; t < vertexCount; t++) {
+				source = graphLinkedList.search(t);
+				for (int j = 0; j < source.neighbors.size(); j++) {
+					Node destination = source.neighbors.get(j).getDestination();
+					int weight = source.neighbors.get(j).getWeight();
+					relax = this.relax(graphLinkedList, source, destination, weight);
+				}//end for j
+			}//end t = 0
+		}//end for i
+	}//end bellmanFord
+	public boolean relax(LinkedList graphLinkedList, Node source, Node destination, int weight) {
 		/*Relax(u,v,w)
 		* 	if v.d > u.d + w(u,v)
 		* 		v.d = u.d + w(u,v)
 		* 		v.π = u */
-		/*if() {
-		
-		}*/
-	}
+		if(destination.distance > (source.distance + weight)) {
+			destination.distance = source.distance + weight;
+			destination.prevVertex = source;
+			return false;
+		}//end if
+		return true;
+	}//end
 	public void printGraphOutput() {
 	
-	}
-}
+	}//end printGraphOutput
+}//end class SingleSourceShortestPath
