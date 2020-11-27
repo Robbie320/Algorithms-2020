78fe6b22e152614393264f2bfe4067ab0d8aeed6
Robbie Perrone <49956805+Robbie320@users.noreply.github.com>
2020-11-27 16:40:35 -0500

    Update Testing.java

--- Semester-long Project/Testing.java
+++ Semester-long Project/Testing.java
@@ -2,7 +2,12 @@ public class Testing {
 	public int test(Integer[] subjects, int start, int groupNumber, int groupSize) {
 		int patient;
 		int infectedNum = 0;
-		for(int t = start; t < (start+groupSize); t++) {
+		int stop = start+groupSize;
+		
+		if(stop > subjects.length) {
+			stop = subjects.length;
+		}
+		for(int t = start; t < stop; t++) {
 			patient = subjects[t];
 			if(patient == 1) { //if subject is infected
 				infectedNum++;
